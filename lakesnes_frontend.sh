583907de972e656f2448d31a9203b224dcd2cffa
Alan Miguel <126789008+Taketoalan@users.noreply.github.com>
2025-12-16 10:06:10 -0500

    Add files via upload

--- lakesnes_frontend.sh
+++ lakesnes_frontend.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-yad --title="LakeSnes v1.1.4" --title-layout=center --width=500 --height=155 --form --width=250 \
- --field="Open Rom":fbtn "$HOME/lakesnes/launcher" \
- --field="Updates":fbtn "firefox https://github.com/Taketoalan/LakeSnes-v1.1.4" \
- --field="About":fbtn "$HOME/lakesnes/about.sh" \
+yad --title="LakeSnes v1.1.4" --title-layout=center --width=500 --height=120 --form --width=250 \
+ --field="Open File":fbtn "$HOME/lakesnes/launcher" \
+ --field="About":fbtn "$HOME/lakesnes/about" \
  --button=gtk-cancel:1 --buttons-layout=center
